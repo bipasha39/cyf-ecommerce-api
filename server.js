fb5389c04f823b330c3933145666448ea9544ab3
bipasha39 <physiobipasha39@gmail.com>
2020-12-18 17:25:30 +0100

    database 2nd week

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const { Pool } = require('pg');
+const app = express();
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'cyf_ecomerece',
+    password: 'migracode',
+    port: 5432
+});
+app.get("/customers", function(req, res) {
+    pool.query('SELECT * FROM customers;', (error,result) => {
+        console.log(result);
+        res.json(result.rows);
+    });
+});
+app.get("/suppliers", function(req, res) {
+    pool.query('SELECT * FROM suppliers;', (error, result) => {
+        res.json(result.rows);
+    });
+});
+app.get("/products", function(req, res) {
+    pool.query(`
+        SELECT p.product_name, s.supplier_name
+        FROM products p
+        JOIN suppliers s
+            ON p.supplier_id = s.id;
+    `, (error, result) => {
+        res.json(result.rows);
+    });
+});
+app.listen(3001, function() {
+    console.log("Server is listening on port 3000. Ready to accept requests!");
+});
